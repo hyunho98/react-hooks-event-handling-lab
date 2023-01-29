1f58be11a0e56b5d7950912d016013fdfb8f54e3
Alex Lee <hyunho98@gmail.com>
2023-01-29 12:38:27 -0500

    commit tree 18ed789f72760679d11f0e9a79262149d3acf9c6

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function focusHandler(event) {
+        console.log('Good!');
+    }
+
+    function blurHandler() {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+    <button onFocus={focusHandler} onBlur={blurHandler}>
+        Eyes on me
+    </button>
+    );
+}
+
+export default EyesOnMe;
